698186b628f8854ea09a3389efc7d406b58fba54
Michael Lamparski <diagonaldevice@gmail.com>
2020-12-25 01:47:46 -0500

    fix the build script rerun directives

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use lalrpop;
 
 fn main() {
-    lalrpop::process_root().unwrap();
+    lalrpop::Configuration::new()
+        .emit_rerun_directives(true)
+        .process_current_dir().unwrap();
 }
