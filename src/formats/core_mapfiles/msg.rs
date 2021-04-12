89dd028288564add1f91fe614221b2253f77c032
Michael Lamparski <diagonaldevice@gmail.com>
2021-04-12 04:30:15 -0400

    add missing MSG signatures from thtk
    Closes #12.  (and opens #28... bleh...)

--- src/formats/core_mapfiles/msg.rs
+++ src/formats/core_mapfiles/msg.rs
@@ -49,6 +49,66 @@ static MSG: &CoreSignatures = &CoreSignatures {
         (Th08, 20, "m"),
         (Th08, 21, "S"),
         (Th08, 22, ""),
+
+        // =========================================
+        // FIXME: Everything beyond this point is copied from thtk,
+        //        which has a track record for being wrong about unused instructions
+        //        and arguments that are always zero in vanilla files.
+        //
+        //        I'll reverse MSG in these games at some point...  - ExpHP
+
+        (Th09, 8, ""),
+        (Th09, 15, "SSS"),
+        (Th09, 23, "S"),
+        (Th09, 24, ""),
+        (Th09, 25, ""),
+        (Th09, 28, "S"),
+
+        // (FIXME: this many changed signatures looks suspicious, don't it?
+        //         This is probably a rewrite of the format, and should be broken
+        //         into a new CoreSignatures if that's the case)
+        (Th10, 1, "S"),
+        (Th10, 2, "S"),
+        (Th10, 3, ""),
+        (Th10, 4, ""),
+        (Th10, 5, ""),
+        (Th10, 7, ""),
+        (Th10, 10, "S"),
+        (Th10, 12, "S"),
+        (Th10, 14, "S"),
+        (Th10, 17, "m"),
+        (Th10, 18, ""),
+        (Th10, 19, ""),
+        (Th10, 20, ""),
+        (Th10, 21, ""),
+        (Th10, 22, ""),
+        (Th10, 23, ""),
+        (Th10, 25, "S"),
+
+        (Th11, 9, ""),
+        (Th11, 10, "S"),
+        (Th11, 11, "S"),
+        (Th11, 12, ""),
+
+        (Th12, 27, "f"),
+
+        (Th128, 28, "ff"),
+        (Th128, 29, "S"),
+        (Th128, 30, ""),
+
+        (Th13, 31, "S"),
+
+        (Th14, 5, "S"),
+        (Th14, 8, "S"),
+        (Th14, 14, "SS"),
+        (Th14, 20, "S"),
+        (Th14, 32, "S"),
+
+        (Th143, 33, "S"),
+
+        (Th16, 34, "SS"),
+        (Th16, 35, ""),
+        // =========================================
     ],
     var: &[],
 };
