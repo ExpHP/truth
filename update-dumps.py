cd0334a030b92dcf4ba9924c9fd7b76b5e87bd6a
Michael Lamparski <diagonaldevice@gmail.com>
2022-07-14 17:59:20 -0400

    make update-dumps.py usable by others

--- update-dumps.py
+++ update-dumps.py
@@ -8,17 +8,20 @@ import os
 import sys
 import time
 import fnmatch
+import warnings
+try:
+    from ruamel.yaml import YAML
+except ImportError:
+    print('Please install ruamel.yaml:', file=sys.stderr)
+    print('    python3 -m pip install ruamel.yaml', file=sys.stderr)
+    sys.exit(1)
+yaml = YAML(typ='safe')
 
 PROG = os.path.basename(sys.argv[0])
+PROG_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# FIXME: Absolute paths in VCS   :shock: :horror:
-SSD_INPUT_DIR = '/home/exp/win/truth-bench/decomp'
-SSD_DUMP_DIR = '/home/exp/win/truth-bench/all-dumps'
-
-INPUT_DIR = '/home/exp/thcrap/decomp'
-INPUT_SUBDIR_PREFIX = 'bleh'
-DUMP_DIR = 'all-dumps'
-DUMP_SUBDIR_PREFIX = 'th'
+DEFAULT_CONFIG_FILENAME = 'update-dumps.yaml'
+EXAMPLE_CONFIG_FILENAME = 'update-dumps-example.yaml'
 
 def main():
     global INPUT_DIR
@@ -31,22 +34,37 @@ def main():
         description='Script used to test recompilation of all files in all games.',
     )
     parser.set_defaults(formats=[], modes=[], game=[], decompile_args=[])
+    parser.add_argument('-c', '--config', type=str, default=None, help=
+        'path to config file. If not provided, will default to reading '
+        f'{DEFAULT_CONFIG_FILENAME} from the directory with this script.'
+    )
     parser.add_argument('-g', '--game', action='append', type=parse_game)
-    parser.add_argument('--compile', action='append_const', dest='modes', const='compile')
-    parser.add_argument('--decompile', action='append_const', dest='modes', const='decompile')
-    parser.add_argument('--anm', action='append_const', dest='formats', const='anm')
-    parser.add_argument('--std', action='append_const', dest='formats', const='std')
-    parser.add_argument('--msg', action='append_const', dest='formats', const='msg')
-    parser.add_argument('--ecl', action='append_const', dest='formats', const='ecl')
-    parser.add_argument('--mission', action='append_const', dest='formats', const='mission')
-    parser.add_argument('--ssd', action='store_true')
+    parser.add_argument('--compile', action='append_const', dest='modes', const='compile', help='restrict to the recompilation step')
+    parser.add_argument('--decompile', action='append_const', dest='modes', const='decompile', help='restrict to the decompilation step')
+    parser.add_argument('--anm', action='append_const', dest='formats', const='anm', help='restrict to anm files. (and any other language flags used)')
+    parser.add_argument('--std', action='append_const', dest='formats', const='std', help='restrict to std files. (and any other language flags used)')
+    parser.add_argument('--msg', action='append_const', dest='formats', const='msg', help='restrict to msg files. (and any other language flags used)')
+    parser.add_argument('--ecl', action='append_const', dest='formats', const='ecl', help='restrict to ecl files. (and any other language flags used)')
+    parser.add_argument('--mission', action='append_const', dest='formats', const='mission', help='restrict to mission files. (and any other language flags used)')
     parser.add_argument('--debug', action='store_false', dest='optimized', help='use unoptimized builds')
-    parser.add_argument('--decompile-arg', dest='decompile_args', action='append')
-    parser.add_argument('--nobuild', action='store_false', dest='build')
-    parser.add_argument('--noverify', action='store_false', dest='verify')
-    parser.add_argument('--update-known-bad', action='store_true')
-    parser.add_argument('-j', type=int, default=1)
+    parser.add_argument('--decompile-arg', dest='decompile_args', action='append', help='supply an argument during decompile commands')
+    parser.add_argument('--nobuild', action='store_false', dest='build', help='do not build truth')
+    parser.add_argument('--noverify', action='store_false', dest='verify', help='do not verify round-tripping (expensive)')
+    parser.add_argument('--update-known-bad', action='store_true', help='bless the new list of bad files')
+    parser.add_argument('-j', type=int, default=1, help='run this many jobs in parallel')
     args = parser.parse_args()
+
+    if args.config is None:
+        default_config_path = os.path.join(PROG_DIR, DEFAULT_CONFIG_FILENAME)
+        if os.path.exists(default_config_path):
+            args.config = default_config_path
+        else:
+            die(
+                f'no config file! Please create {DEFAULT_CONFIG_FILENAME}. '
+                f'See {EXAMPLE_CONFIG_FILENAME} for an example.'
+            )
+
+    config = Config.from_path(args.config)
 
     # if custom filters were given then we won't end up iterating over all files
     badfiles_is_comprehensive = args.verify and not any([args.game, args.formats, args.modes])
@@ -59,11 +77,7 @@ def main():
     if not args.modes:
         args.modes = list(ALL_MODES)
 
-    if args.ssd:
-        INPUT_DIR = SSD_INPUT_DIR
-        DUMP_DIR = SSD_DUMP_DIR
-
-    games = [x[len(INPUT_SUBDIR_PREFIX):] for x in os.listdir(INPUT_DIR) if x.startswith(INPUT_SUBDIR_PREFIX)]
+    games = find_all_games(config)
     all_files = []
     badfiles = [] if args.verify else None
     timelog = {
@@ -74,9 +88,9 @@ def main():
     for game in games:
         if not any(lo <= game <= hi for (lo, hi) in args.game):
             continue
-        for filename in os.listdir(f'{INPUT_DIR}/{INPUT_SUBDIR_PREFIX}{game}'):
+        for filename in os.listdir(config.directories.input.for_game(game)):
             if get_format(game, filename) in args.formats:
-                all_files.append((game, filename, timelog, badfiles, args.modes, args.optimized, args.decompile_args))
+                all_files.append((game, filename, timelog, badfiles, args.modes, args.optimized, args.decompile_args, config))
 
     if args.build:
 
@@ -84,7 +98,7 @@ def main():
     with ThreadPool(args.j) as p:
         p.starmap(process_file, all_files)
 
-    known_bad_path = f'{DUMP_DIR}/known-bad'
+    known_bad_path = f'{config.directories.binary_dump.root}/known-bad'
     update_known_bad = args.update_known_bad
     if os.path.exists(known_bad_path):
         known_bad = set(x.strip() for x in open(known_bad_path))
@@ -166,43 +180,43 @@ def get_format(game, path):
     return None
 
 # ABORT = 0
-def process_file(game, file, timelog, badfiles, modes, optimized, extra_decompile_args):
+def process_file(game, file, timelog, badfiles, modes, optimized, extra_decompile_args, config):
     # global ABORT
 
-    input = f'{INPUT_DIR}/{INPUT_SUBDIR_PREFIX}{game}/{file}'
-    outspec = f'{DUMP_DIR}/{DUMP_SUBDIR_PREFIX}{game}/{file}.spec'
-    outfile = f'{DUMP_DIR}/{DUMP_SUBDIR_PREFIX}{game}/{file}'
+    input = config.directories.input.for_game(game) + f'/{file}'
+    outspec = config.directories.text_dump.for_game(game) + f'/{file}.spec'
+    outfile = config.directories.binary_dump.for_game(game) + f'/{file}'
 
     bindir = f'target/' + ('release' if optimized else 'debug')
 
     format = get_format(game, file)
     if format == 'std':
-        decompile_args = [f'{bindir}/trustd', 'decompile', '-g', game, input] + extra_decompile_args
+        decompile_args = [f'{bindir}/trustd', 'decompile', '-g', game, input, '-o', outspec] + extra_decompile_args
         compile_args = [f'{bindir}/trustd', 'compile', '-g', game, outspec, '-o', outfile]
     elif format == 'anm':
-        decompile_args = [f'{bindir}/truanm', 'decompile', '-g', game, input] + extra_decompile_args
+        decompile_args = [f'{bindir}/truanm', 'decompile', '-g', game, input, '-o', outspec] + extra_decompile_args
         compile_args = [f'{bindir}/truanm', 'compile', '-g', game, outspec, '-i', input, '-o', outfile]
     elif format == 'msg':
-        decompile_args = [f'{bindir}/trumsg', 'decompile', '-g', game, input] + extra_decompile_args
+        decompile_args = [f'{bindir}/trumsg', 'decompile', '-g', game, input, '-o', outspec] + extra_decompile_args
         compile_args = [f'{bindir}/trumsg', 'compile', '-g', game, outspec, '-o', outfile]
     elif format == 'mission':
-        decompile_args = [f'{bindir}/trumsg', 'decompile', '--mission', '-g', game, input] + extra_decompile_args
+        decompile_args = [f'{bindir}/trumsg', 'decompile', '--mission', '-g', game, input, '-o', outspec] + extra_decompile_args
         compile_args = [f'{bindir}/trumsg', 'compile', '--mission', '-g', game, outspec, '-o', outfile]
     elif format == 'ecl':
-        decompile_args = [f'{bindir}/truecl', 'decompile', '-g', game, input] + extra_decompile_args
+        decompile_args = [f'{bindir}/truecl', 'decompile', '-g', game, input, '-o', outspec] + extra_decompile_args
         compile_args = [f'{bindir}/truecl', 'compile', '-g', game, outspec, '-o', outfile]
     else:
         assert False, file
 
     os.makedirs(os.path.dirname(outspec), exist_ok=True)
+    os.makedirs(os.path.dirname(outfile), exist_ok=True)
 
     # try:
     if 'decompile' in modes:
-        with open(outspec, 'w') as f:
-            print(' '.join(map(shlex.quote, decompile_args)))
-            start_time = time.time()
-            subprocess.run(decompile_args, stdout=f, check=True)
-            timelog[f'{format}-decompile'] += time.time() - start_time
+        print(' '.join(map(shlex.quote, decompile_args)))
+        start_time = time.time()
+        subprocess.run(decompile_args, check=True)
+        timelog[f'{format}-decompile'] += time.time() - start_time
 
     if 'compile' in modes:
         print(' '.join(map(shlex.quote, compile_args)))
@@ -218,6 +232,23 @@ def process_file(game, file, timelog, badfiles, modes, optimized, extra_decompil
     #     ABORT = 1
     #     raise
 
+def find_all_games(config):
+    input_dirs = config.directories.input
+    games = [
+        x[len(input_dirs.subdir_prefix):]
+        for x in os.listdir(input_dirs.root)
+        if x.startswith(input_dirs.subdir_prefix)
+    ]
+
+    suspicious_games = [g for g in games if g not in MSG_GLOBS]
+    if suspicious_games:
+        supicious_str = ', '.join(f'{input_dirs.subdir_prefix}')
+        warnings.warn(
+            f'unrecognized game numbers: {suspicious_str}\n'
+            'Maybe support hasn\'t been added. Make sure all game numbers < 10 are zero-padded!'
+        )
+
+    return sorted(set(games) - set(suspicious_games))
 
 def parse_game(s):
     if ':' in s:
@@ -226,6 +257,55 @@ def parse_game(s):
     else:
         return s, s
 
+
+# ------------------------------------------------------
+
+class Config:
+    def __init__(self, d):
+        if not isinstance(d, dict):
+            die(f'at config root: expected dict, got {type(d)}')
+
+        d = dict(d)
+        self.directories = ConfigDirectories(d.pop('directories'), self_key='directories')
+
+        for key in d:
+            warnings.warn(f'unrecognized key {repr(key)} in config')
+
+    @classmethod
+    def from_path(cls, path):
+        with open(path) as f:
+            d = yaml.load(f)
+        return cls(d)
+
+class ConfigDirectories(dict):
+    def __init__(self, d, self_key):
+        if not isinstance(d, dict):
+            die(f'at config key {repr(self_key)}: expected dict, got {type(d)}')
+
+        d = dict(d)
+        self.input = ConfigDirectory(d.pop('input'), self_key=f'{self_key}.input')
+        self.text_dump = ConfigDirectory(d.pop('text-dump'), self_key=f'{self_key}.text-dump')
+        self.binary_dump = ConfigDirectory(d.pop('binary-dump'), self_key=f'{self_key}.binary-dump')
+
+        for key in d:
+            full_key = f'{self_key}.{key}'
+            warnings.warn(f'unrecognized key {repr(full_key)} in config')
+
+class ConfigDirectory:
+    def __init__(self, d, self_key):
+        if not isinstance(d, dict):
+            die(f'at config key {repr(self_key)}: expected dict, got {type(d)}')
+
+        d = dict(d)
+        self.root = d.pop('root')
+        self.subdir_prefix = d.pop('subdir-prefix')
+
+        for key in d:
+            full_key = f'{self_key}.{key}'
+            warnings.warn(f'unrecognized key {repr(full_key)} in config')
+
+    def for_game(self, game):
+        return f'{self.root}/{self.subdir_prefix}{game}'
 
 # ------------------------------------------------------
 
